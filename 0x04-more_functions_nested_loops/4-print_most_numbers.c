0a26ccba36240be16a79c9258a66d3550814538b
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-15 22:24:30 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,24 +2,25 @@
 #include <unistd.h>
 
 /**
-* print_most_numbers - check main
-* Description: Prints numbers from 0 to 9 except 2 and 4
-* Return: Nothing
-**/
+ * print_most_numbers - check main
+ * Description: Write a function that prints the numbers, from 0 to 9
+ * Return: Nothing
+ */
 
 void print_most_numbers(void)
 {
-int c;
-for (c = 0; c <= 9; c++)
-{
-if ((c != 2) || (c != 4))
-{
-_putchar(c + '0');
-}
-else
-{
-continue;
-}
-}
-_putchar("\n");
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 || c != 4)
+		{
+      _putchar(c + '0');
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
 }
