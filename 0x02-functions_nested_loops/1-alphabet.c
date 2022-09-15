eff6dac6a215daf16f3288e96119b9e8ae5a9234
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-15 11:17:47 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,9 @@
 void print_alphabet(void)
 {
 char al;
-for (al = 'a'; al <= 'z'; al++)
-{
-_putchar(al);
-}
-  _putchar('\n');
+    for (al = 'a'; al <= 'z'; al++)
+    {
+      _putchar(al);
+    }
+      _putchar('\n');
 }
