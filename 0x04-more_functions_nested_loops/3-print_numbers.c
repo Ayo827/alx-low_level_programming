1c0c695f4d2b0a460fa74d4dc570bef01cd2446c
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-15 11:30:12 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_numbers - check main
+* Description: Print  numbers from 0 to 9
+* Return: nothing
+**/
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar("%d", i);
+}
+_putchar("\n");
+}
