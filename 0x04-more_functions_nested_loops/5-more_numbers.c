944dd5aa71fc46f9366c7cb852cd79cce8cbd1d1
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-15 15:08:32 -0700

    Print 10x the number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,9 @@
+#include main.h
+#include <unistd.h>
+/**
+* more_numbers - check main
+* Description: Write a function that prints 10x the numbers from 0 to 14
+* Return: Nothing
+*/
+void more_numbers(void)
+{
