78e72a2c8ccdbbcb29e315c59adf3634c0a8b3d1
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-15 03:47:15 -0700

    print x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - No entry point
+*
+* Return: On success Nothing
+**/
+void print_alphabet_x10(void)
+{
+char alpha;
+int i;
+for (i = 0; i <= 9; i++)
+{
+for (alpha = "a"; alpha <= "z"; alpha++)
+{
+_putchar(alpha);
+}
+putchar('\n');
+}
+}
