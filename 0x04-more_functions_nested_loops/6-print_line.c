5dc2074f49e0a1c3eee0cba394ad7e1f136482b9
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-16 00:16:50 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -15,12 +15,12 @@ void print_line(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('_');
+			_putchar("_");
 		}
-		_putchar('\n');
+		_putchar("\n");
 	}
 	else
 	{
-	_putchar('\n');
+	_putchar("\n");
 	}
 }
