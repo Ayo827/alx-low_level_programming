5bd16f8289638d037063012a037f81e6f69c2706
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-15 15:36:28 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* print_line - check main
+* @n: an integer input
+* Description: function that draws a straight line
+* Return: Nothing
+**/
+void print_line(int n)
+{
+int i;
