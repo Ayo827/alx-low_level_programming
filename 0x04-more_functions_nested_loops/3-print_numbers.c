40a7ba8b717e555bc1b2ca22d81167d39d4f908f
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-15 19:44:47 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
 #include <unistd.h>
+
 /**
-* print_numbers - check main
-* Description: Print  numbers from 0 to 9
-* Return: Nothing
-**/
+ * print_numbers - check main
+ * Description: Write a function that prints the numbers, from 0 to 9
+ * Return: Nothing
+ */
+
 void print_numbers(void)
 {
-int i;
-for (i = 0; i <= 9; i++)
-{
-_putchar("%d", i);
-}
-_putchar("\n");
-return (0);
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
 }
