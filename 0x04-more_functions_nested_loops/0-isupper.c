83858943844e9c18d908d3ea89a4767e9da261ae
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-15 09:49:46 -0700

    Check for uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _isupper - check main
+* @c - is an integer input n
+* Description - Checks for uppercase character
+* Return - 1 if uppercase else 0
+**/
+int _isupper(int c)
+{
+char ch;
+int n = 0;
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+if (ch == c)
+{
+n = 1;
+break;
+}
+}
+return (n);
+}
