2a98ddfe4b87fa19c9bebc62bac2ac324fbedc36
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-15 10:43:26 -0700

    Comapring numbers 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _isdigit - check main
+* @c: is an integer input n
+* Description: returns 1 if digit else 0
+* Return: 0 or 1
+**/
+int _isdigit(int c)
+{
+int a;
+int n = 0;
+for (a = 0; a < 10; a++)
+{
+if (a == c)
+{
+n = 1;
+break;
+}
+}
+return (n);
+}
