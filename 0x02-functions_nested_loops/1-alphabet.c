b24208a1f7b6e1f8d491d5350e6c8fbff71778d3
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-15 03:11:31 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*  print_alphabet - No entry point
+* Return: On success Nothing.
+**/
+void print_alphabet(void)
+{
+char al;
+for (al = a; al <= z; al++)
+{
+_putchar(al);
+}
+}
