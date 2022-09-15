9a18705143e7834e76ee9d3202e5e857f58168f2
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-15 23:09:50 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,32 @@
-#include main.h
-#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
 /**
-* more_numbers - check main
-* Description: Write a function that prints 10x the numbers from 0 to 14
-* Return: Nothing
-*/
+ * more_numbers - check main
+ *
+ * Description: function that prints 10 times the numbers, from 0 to 14
+ * Return: Nothing
+ */
+
 void more_numbers(void)
 {
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+		printf("\n");
+	}
+}
