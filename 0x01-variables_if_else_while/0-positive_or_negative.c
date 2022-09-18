30217fb8f25cdd64572b0f1012c7f428b283ecb7
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-18 13:06:22 -0700

    Positive or negative or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* positive_or_negative - check main
+* Description: Check if value is positive or negative or zero
+* Return: (0) Success
+**/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf('is positive\n');
+}
+else if (n < 0)
+{
+printf('is negative');
+}
+else
+{
+printf('is zero\n')
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This directory contains conditional codes in C
