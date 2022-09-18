63bf88ca1e4362d3e4f97173f931d9d82dab41ec
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-18 22:38:42 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
 * main - check description
 * Description: Print alphabets in lowercase and uppercase
 * Return: 0
 **/
-
 int main(void)
 {
-  char A;
-  for (A = 'a'; A <= 'z'; A++)
-  {
-    putchar(A);
-  }
-  for(A = 'A'; A <= 'Z'; A++)
-  {
-    putchar(A);
-  }
-  putchar('\n');
-  return (0);
+char A;
+for (A = 'a'; A <= 'z'; A++)
+{
+putchar(A);
+}
+for(A = 'A'; A <= 'Z'; A++)
+{
+putchar(A);
+}
+putchar('\n');
+return (0);
 }
