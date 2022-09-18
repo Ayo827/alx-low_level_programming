4935d99ad0473f9eaef9ac6c07596011080fa5ed
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-18 21:12:16 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf('is positive\n');
+printf("is positive\n");
 }
 else if (n < 0)
 {
-printf('is negative');
+printf("is negative\n");
 }
 else
 {
-printf('is zero\n')
+printf("is zero\n")
 }
 return (0);
 }
