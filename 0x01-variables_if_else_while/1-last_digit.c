0853050bbeec24277735d1626f6ab90b5752ce45
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-18 21:53:30 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,13 +11,13 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n[1] > 5)
+if (n[-1] > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, n[1]);
+printf("Last digit of %d is %d and is greater than 5", n, n[-1]);
 }
-else if (n < 6 && n != 0)
+else if (n[-1] < 6 && n[-1] != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and 0", n, n[1]);
+printf("Last digit of %d is %d and is less than 6 and 0", n, n[-1]);
 }
 else
 {
