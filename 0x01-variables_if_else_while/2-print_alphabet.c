d25e193c6a964535c4db38f21681c82fbc5a0c8e
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-18 14:23:22 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Check main
+* Description: Print all alphabets
+Return: (0)
+int main(void)
+{
+char A;
+for (A = a; A <= z; A++)
+{
+putchar("%c", A);
+}
+putchar("\n")
+return (0);
+}
