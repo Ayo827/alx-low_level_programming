833242196dcf980bee66aba00210f8e0c1a1234b
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-18 22:35:07 +0100

    Print lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib>
+#include <stdio>
+
+/**
+* main - check description
+* Description: Print alphabets in lowercase and uppercase
+* Return: 0
+**/
+
+int main(void)
+{
+  char A;
+  for (A = 'a'; A <= 'z'; A++)
+  {
+    putchar(A);
+  }
+  for(A = 'A'; A <= 'Z'; A++)
+  {
+    putchar(A);
+  }
+  putchar('\n');
+  return (0);
+}
