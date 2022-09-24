87934cce8973453aa0553084e6da12a4594295d8
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 12:29:17 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strlen - Length of a string
+* @str: Is a string in double quotes
+* Description: Prints string
+* Return: returns (0)
+**/
+void _puts(char *str)
+{
+puts(str);
+puts("\n");
+return (0);
+}
