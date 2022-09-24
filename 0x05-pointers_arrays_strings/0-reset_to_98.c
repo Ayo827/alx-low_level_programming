a0463167ead2f36655673a38600a9dff753a2ff7
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 03:35:21 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,9 @@
+#include "main.h"
 #include <stdio.h>
 /**
-* main - check the code
+* reset_to_98 - check the code
 * Return: Always 0
 **/
-void reset_to_98(int *n);
-int main(void)
-{
-int n;
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
-}
 void reset_to_98(int *n)
 {
 *n = 98;
