ccb1d10114a44072fd111d4c717cc41daf16e9f7
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 12:58:46 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* print_rev - Check the code
+* @s: Is a string
+* Description: Prints string in reverse
+* Return: string
+**/
+void print_rev(char *s)
+{
+int i;
+size_t s_length;
+s_length = strlen(s);
+char new_s;
+new_s = "";
+for (i = 1; i <= s_length; i++)
+{
+new_s = new_s + s[s_length - i];
+}
+puts(new_s);
+}
