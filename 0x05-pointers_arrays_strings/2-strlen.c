6a247c082c2faa1c9d75f49bb7768f7a6f630d1d
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 04:02:33 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strlen - Length of a string
+* @str: This is string in double quotes
+* return: always (0)
+**/
+void _strlen(char *s)
+{
+size_t strlen(s);
+}
