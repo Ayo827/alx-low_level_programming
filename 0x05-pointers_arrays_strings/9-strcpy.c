2f67c0035a046215f25891dcd3b673ed60b11083
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 16:46:23 +0100

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strcpy - Check the code
+* @dest: Is the destination to copy to
+* @src: Is the source to copy
+* Return: pointer to dest
+**/
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (*dest);
+}
