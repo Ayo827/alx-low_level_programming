f2126ca880844de21a9c8008364f4d601399e75d
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 14:26:50 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,9 @@ void puts2(char *str)
 {
 int i, len;
 len = strlen(str);
-for (i = 0; i < len / 2; i++)
+for (i = 1; i <= len; i++)
 {
-if((i % 2 != 0) || (i == 0))
+if( i % 2 != 0 )
 {
 printf("%c", str[i]);
 }
