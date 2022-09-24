978241b0a5a546c8a636d791a8abb833b49a89ce
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 12:12:09 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,7 @@
 * _strlen - Length of a string
 * @s: Is a string in double quotes
 * Description: Get length of string and return the value
-* return: returns string length
+* Return: returns string length
 **/
 int _strlen(char *s)
 {
