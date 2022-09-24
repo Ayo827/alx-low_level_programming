e8765f5b156237302dfce2a81032841524f0fa70
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 14:09:41 +0100

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* puts2 - Check the code
+* @str: Is a string
+* Description: Prints every other character in a string then newline
+* Return: string
+**/
+void puts2(char *str)
+{
+int i, len;
+len = strlen(str);
+for (i = 0; i < len / 2; i++)
+{
+puts(str[i]);
+}
+}
