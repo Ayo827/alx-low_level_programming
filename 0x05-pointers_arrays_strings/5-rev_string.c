e49123d3169799b9d0aa88ff7a9870077b1ed7b9
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-24 14:03:11 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,12 @@
 **/
 void rev_string(char *s)
 {
-int i, len, temp;  
-len = strlen(s);  
-for (i = 0; i < len/2; i++)  
-{   
+int i, len, temp;
+len = strlen(s);
+for (i = 0; i < len / 2; i++)
+{
 temp = s[i];
 s[i] = s[len - i - 1];
-s[len - i - 1] = temp;  
+s[len - i - 1] = temp;
 }
 }
