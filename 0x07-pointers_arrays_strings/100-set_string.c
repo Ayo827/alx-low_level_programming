0879b15c327985001add886cbf9a9a520b7ad26b
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-26 19:13:52 +0100

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* set_string - Check the code
+* @s: pointer-to-pointer
+* @to: pointer
+* Description: pass pointer value to char
+* Return: 0
+**/
+void set_string(char **s, char *to)
+{
+**s = *to
+}
