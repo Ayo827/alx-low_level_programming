170f1affb1e601bed69168dbcd9a3338529c8db9
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-26 17:59:40 +0100

    prints a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* print_chessboard - Check the code
+* Description: prints a chessboard
+* Return: 0
+**/
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]);
+}
+printf("\n");
+}
+}
