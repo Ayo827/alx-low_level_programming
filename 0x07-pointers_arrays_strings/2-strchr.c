c5e8a84c4f1b034438b34f01d35ee0817a9b3fc1
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-26 17:19:08 +0100

    Checks for first occurrence of character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* _memset - Check the code
+* @s: This is a pointer string
+* @c: This is a string
+* Description: checks for the first occurrence of character
+* Return: pointer to character
+**/
+char *_strchr(char *s, char c)
+{
+return (strchr(s, c));
+}
