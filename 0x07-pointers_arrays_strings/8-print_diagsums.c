779cb4d90bd249473e9c6a0c7f2081c3a7671d8b
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-09-26 18:35:59 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,18 +1,21 @@
+#include "main.h"
 #include <string.h>
 #include <stdio.h>
-#include "main.h"
 /**
 * print_diagsums - Check the code
 * @a: pointer to array
-* @size: size of array
+* @size: size is an integer
 * Description: prints sum of diagonal
 * Return: 0
 **/
 void print_diagsums(int *a, int size)
 {
-int i, j;
-int firstDiagonal = 0;
-int secondDiagonal = 0;
+int i;
+int j;
+int firstDiagonal;
+firstDiagonal = 0
+int secondDiagonal;
+secondDiagonal = 0;
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
