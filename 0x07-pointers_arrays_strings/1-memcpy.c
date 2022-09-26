4d670fcf711e60e2d4d974e334c21caf2592af82
Ayodeji Adebola <dopeman827@gmail.com>
2022-09-26 09:09:43 -0700

    Copy from memory and return pointer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+include "main.h"
+/**
+* _memcpy - check code
+*  @dest: a string pointer
+* @src: a string pointer
+* @n: an unsigned integer
+Description: copies n bytes and returns dest pointer
+* Return: dest pointer
+**/
+char *_memcpy(char *dest, char *src, unsigned int n)
