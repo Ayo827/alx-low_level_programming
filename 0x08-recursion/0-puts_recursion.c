ad32dd740c8cc65330f48498cd1da4485a2bf056
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-03 14:51:36 +0100

    prints string with puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include 'main.h'
+/**
+*  _puts_recursion - check the code
+*  Description: prints a string using puts
+*  @s: A string parameter
+*  Return: Always 0.
+*/
+ void _puts_recursion(char *s)
+ {
+     puts(s);
+ }
