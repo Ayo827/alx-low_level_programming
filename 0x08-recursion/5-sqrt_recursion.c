15b3594646b62bf4ccab7e1e4f636aea23051cb3
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-04 17:05:21 +0100

    Add files via upload

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+
+/**
+* _sqrt_recursion - check the code
+* @x: integer parameter
+* Description: return square root of x
+* Return: integer
+**/
+int _sqrt_recursion(int x)
+{
+float y;
+float s;
+int v;
+y = (float) x;
+s = sqrt(y);
+v = (int) s;
+if ((s - v) == 0.000000)
+{
+return (v);
+}
+else
+{
+return (-1);
+}
+}
