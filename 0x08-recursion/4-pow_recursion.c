3d88959fc93bc7e3e687864fa74f5226b1d69c98
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-04 14:22:41 +0100

    Power of x

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+/**
+* _pow_recursion - check the code
+* @x: integer parameter
+* @y: integer parameter
+* Description: return x**y
+* Return: integer
+**/
+int _pow_recursion(int x, int y){
+return pow(x, y);
+}
