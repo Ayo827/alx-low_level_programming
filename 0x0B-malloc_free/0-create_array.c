274424fb13f5ef76ee5f5a9e481da821349aa721
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-05 12:50:39 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 /**
 * create_array - check the code
@@ -11,7 +10,6 @@
 **/
 char *create_array(unsigned int size, char c)
 {
-int length = strlen(c);
 if (size == 0)
 {
 return (NULL);
