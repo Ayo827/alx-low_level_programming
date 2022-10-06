b11b9cdabfa1b3d96994bd78ccb36b211bebfe80
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-06 17:31:31 +0100

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * alloc_grid - Check description 
+ * @width: width of the grid
+ * @height: height of the grid
+ * Description: Creates a 2-D array and returns pointer
+ * Return: pointer.
+ **/
+void **alloc_grid(int width, int height)
+{
+int w;
+int h;
+int **grid;
+char *a;
+h = 0;
+a = malloc(INT_MAX);
+if ((a == NULL) || (width <= 0) || (height <= 0))
+{
+return (NULL);
+}
+a = malloc(w * h);
+while (h < height)
+{
+w = 0;
+while (w < width)
+{
+grid[h][w];
+w++;
+}
+h++;
+}
+a = grid;
+return (a);
+}
