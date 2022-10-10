667aee55aacc7b3026442f9758ba7c74a911a89a
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-10 22:39:24 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
-typedef struct dog {char *name; float age; char *owner;} dog_t;
+struct dog {char *name; float age; char *owner;};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-// typedef struct dog dog_t;
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
