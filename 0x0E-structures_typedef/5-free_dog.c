18e7559ecb6dca8d1a89616de56a9a283f1ab88f
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-10 21:50:37 +0100

    Free memory space

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+* free_dog - check the code
+* @d: pointer to struct
+* Description: free the memory
+* Return: nothing
+**/
+void free_dog(dog_t *d)
+{
+d = malloc(sizeof(dog_t));
+free();
+}
