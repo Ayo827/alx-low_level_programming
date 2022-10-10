03b9277ea55436c237d3ab00eac944449d929159
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-10 16:55:41 +0100

    Print out the elements in struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+* print_dog - check the code
+* @d: Pointer to struct
+* Description: Print out struct
+* Return: nothing
+**/
+void print_dog(struct dog *d)
+{
+float age = d->age;
+if (d == NULL)
+{
+    printf("");
+}
+else if ((d->name != NULL) || (age >= 0.000000 ) || (d->owner != NULL))
+{
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
+else if ((d->name == NULL) && (d->age > 0.000000) && (d->owner != NULL))
+{
+printf("Name: (nil)\n");
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
+else if ((d->name != NULL) && (d->age < 0.000000) && (d->owner != NULL))
+{
+printf("Name: %s\n", d->name);
+printf("Age: (nil)\n");
+printf("Owner: %s\n", d->owner);
+}
+else if ((d->name != NULL) && (d->age >= 0.000000) && (d->owner == NULL))
+{
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: (nil)\n");
+}
+else if ((d->name == NULL) && (d->age < 0.000000) && (d->owner != NULL))
+{
+printf("Name: (nil)\n");
+printf("Age: (nil)\n");
+printf("Owner: %s\n", d->owner);
+}
+else if ((d->name == NULL) && (d->age >= 0.000000) && (d->owner == NULL))
+{
+printf("Name: (nil)\n");
+printf("Age: %f\n", d->age);
+printf("Owner: (nil)\n");
+}
+else if ((d->name != NULL) && (d->age < 0.000000) && (d->owner == NULL))
+{
+printf("Name: %s\n", d->name);
+printf("Age: (nil)\n");
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Name: (nil)\n");
+printf("Age: (nil)\n");
+printf("Owner: (nil)\n");
+}
+}
