bd3c9702afa7ae898800851372625edfee332508
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-10 22:06:16 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "dog.h"
 /**
 * free_dog - check the code
@@ -9,6 +10,8 @@
 **/
 void free_dog(dog_t *d)
 {
+d = malloc(sizeof(INT_MAX));
+if (d == NULL) return (NULL);
 d = malloc(sizeof(dog_t));
 free();
 }
