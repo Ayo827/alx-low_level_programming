20012f2007c76bd4233c35bfd807d2f93307c43c
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-12 00:28:52 +0100

    Print name of file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - check the code
+* Description: prints name of file
+* Return: Always 0
+**/
+int main(int argc, char* argv)
+{
+printf("%s", __FILE__);
+return (0);
+}
