db9c26dd8f0a55e82d273b16ea5d0c8ca94dc16a
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-11 14:36:38 +0100

    Print name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - check code
+* 
+* Description: Prints file name
+* Return: Nothing
+**/
+
+int main(void)
+{
+printf("%s", __FILE__);
+return (0);
+}
