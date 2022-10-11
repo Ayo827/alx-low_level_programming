5bcf441af8a6690788b1348c0225c7d4996b0722
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-12 00:58:50 +0100

    Print number of argument in array

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - check the code
+* @argc: integer arguememt
+* @argv: array pointer
+* Description: Print nmber of argument
+* Return: 0
+**/
+int main(int argc, char *agrv[])
+{
+printf("%d\n", argc);
+return (0);
+}
