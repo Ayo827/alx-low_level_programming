144ae869f4215116b236c54af919fde0a4c64613
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-14 00:36:57 +0100

    Print items in an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+* array_iterator - check code
+* @array: array of integers
+* @size: size of array
+* Description: Prints elements of an array
+* Return: Always 0
+**/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+for (i = 0; i < size; i++) action(array[i]);
+return (0);
+}
