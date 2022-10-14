05ab156b050f6dc1d963a65a4baa6344933051c0
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-14 01:18:22 +0100

    Returns index of elements greater than zero

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+* int_index - check code
+* @array: array of integers
+* @size: size of array
+* @cmp: pointer to function
+* Description: Returns index of element > 0
+* Return: Always index
+**/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+if (i >= size)
+{
+return (-1);
+}
+}
