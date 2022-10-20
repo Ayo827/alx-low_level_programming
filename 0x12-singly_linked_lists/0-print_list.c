7233189d48d080e43a0add0ba96b2e041d235dda
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-20 20:55:30 +0100

    Fixed error in printing node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,24 @@
 #include "lists.h"
+
 /*
 * print_list - check code
 * @h: pointer to list
 * Description: Function to return number of node
 * Return: integer
 */
+int counter(int i)
+{
+return (i + 1);
+}
+
 size_t print_list(const list_t *h)
 {
 int i;
-i = 1;
-while(h->next != NULL)
+i = 0;
+while(h != NULL)
 {
-i++;
+i = counter(i);
+h = h->next;
 }
 return (i);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,5 +15,4 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-_putchar(char* c);
 size_t print_list(const list_t *h);
