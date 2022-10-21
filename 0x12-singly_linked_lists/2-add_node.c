c688d8b436fd3779e6455e1ce1190d7a82c2cb97
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2022-10-21 17:16:45 +0100

    Fixed errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,13 +12,11 @@ list_t *new;
 new = malloc(sizeof(list_t));
 if (new == NULL)
 {
-printf("Error\n");
 return (NULL);
 }
 new->str = strdup(str);
 new->len = strlen(str);
 new->next = *head;
-*head = new;
 printf("[%lu] %s\n", (long unsigned int)new->len, new->str);
 return (new->next);
 }
